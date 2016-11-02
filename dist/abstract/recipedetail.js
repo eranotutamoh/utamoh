05205f17aea9cb8b351369fa7aacd8f4003ca445
eranotutamoh <jellynull@gmail.com>
2016-11-02 20:45:12 +1300

    End of day

---
+++ dist/abstract/recipedetail.js
@@ -0,0 +1,13 @@
+"use strict";
+var RecDetail = (function () {
+    function RecDetail() {
+    }
+    return RecDetail;
+}());
+exports.RecDetail = RecDetail;
+var Ingredients = (function () {
+    function Ingredients() {
+    }
+    return Ingredients;
+}());
+//# sourceMappingURL=recipedetail.js.map
--- dist/app.module.js
+++ dist/app.module.js
@@ -11,6 +11,8 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var forms_1 = require('@angular/forms');
+var app_routing_module_1 = require('./app-routing.module');
+var http_1 = require('@angular/http');
 var app_component_1 = require('./app.component');
 var recipelink_component_1 = require('./component/recipelink.component');
 var recipes_component_1 = require('./component/recipes.component');
@@ -23,7 +25,9 @@ var AppModule = (function () {
     AppModule = __decorate([
         core_1.NgModule({
             imports: [platform_browser_1.BrowserModule,
-                forms_1.FormsModule
+                forms_1.FormsModule,
+                app_routing_module_1.AppRoutingModule,
+                http_1.HttpModule
             ],
             declarations: [app_component_1.AppComponent,
                 recipelink_component_1.RecLinkComp,
